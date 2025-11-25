3085dd62dd6dbdee8791d3e26a254f68569df93e
varuntej4512 <mvaruntejreddy@gmail.com>
2025-11-25 15:38:06 +0530

    Add files via upload

---
+++ hello.c
@@ -0,0 +1,14 @@
+//SAMPLE PROGRAM
+#inclde<stdio.h>
+int main ()
+{
+	printf("\nvarun tej reddy");
+	printf("\nnravindra reddy");
+	printf("\naruna reddy");
+	printf ("\nnnarasaraopet");
+	return 0;
+}
+
+
+
+
