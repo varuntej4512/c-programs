7df6255a47fec9e218197b17e5425701cf39c306
varuntej4512 <mvaruntejreddy@gmail.com>
2025-12-02 15:49:41 +0530

    Add files via upload

---
+++ strcpy.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+	char s1[20],s2[20];
+	printf("enter string1 name");
+	scanf("%d",&s1);
+	strcpy(s2,s1);
+	printf("original =%s",s1);
+	printf("\n copied string=%s",s2);
+	return 0
+}
